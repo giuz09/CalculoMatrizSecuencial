2472e3836d8df1a65d7ad52e9b0b8909b3012767
giuz <guly_09_@hotmail.com>
2018-11-20 19:11:05 -0300

    calculo secuencial matriz

---
+++ src/calculoMatrizSecuencial.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class calculoMatrizSecuencial {
+	public static void main(String[] args) {
+	//declaracion variables
+	Integer N = 4;
+	Integer i;
+	Integer j;
+	double sum;
+	double [][] matrizA = new double[N][N];
+	double [] B = new double[N];
+	double [] C = new double[N];
+	double [] D = new double[N];
+	
+	///////////////inicializa el matriz
+	Random r = new Random();
+	for (i= 0; i<N; ++i) {
+		for (j= 0; j<N; ++j) {
+		    matrizA[i][j]=r.nextDouble();
+		}
+		B[i]=r.nextDouble();
+	}
+	///////////////comienza el calculo
+	for (i= 0; i<N; ++i) {
+		C[i]=0.0;
+		for (j= 0; j<N; ++j) {
+		    C[i]=C[i]+matrizA[i][j]*B[j];
+		}
+	}
+	sum = 0.0;
+	for (i= 0; i<N; ++i) {
+		D[i]=0.0;
+		for (j= 0; j<N; ++j) {
+		    D[i]=D[i]+matrizA[i][j]*C[j];
+		}
+		sum = sum+D[i]*C[i];
+	}
+	//////////////resultados
+	System.out.println("X es "+sum);
+	
+	}	
+}
